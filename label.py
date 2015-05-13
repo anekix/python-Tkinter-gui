2d1a04d476536f2502cce76ec4210daa410715d3
anekix <kshitij22@live.com>
2015-05-13 11:14:24 -0700

    Create label.py

---
+++ label.py
@@ -0,0 +1,10 @@
+from Tkinter import *
+# while working under Python 3, comment the previous line and uncomment the following line
+#from tkinter import *
+
+root = Tk()
+
+w = Label(root, text="Hello Tkinter!")
+w.pack()
+
+root.mainloop()
